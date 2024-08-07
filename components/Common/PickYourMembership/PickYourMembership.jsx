45e5185841b1eea939d22a6d90733d8b70c98ac0
faisal-aali <faz.ali.bhamani@gmail.com>
2024-08-08 00:40:47 +0500

    add subscription detail screen

--- components/Common/PickYourMembership/PickYourMembership.jsx
+++ components/Common/PickYourMembership/PickYourMembership.jsx
@@ -1,7 +1,5 @@
 "use client";
-
 import { useEffect, useState } from "react";
-import { Button } from "@mui/material";
 import axios from 'axios'
 import { useSession } from "next-auth/react";
 import styles from "./PickYourMembership.module.css";
@@ -13,86 +11,76 @@ const PickYourMembership = ({ plan, onBack, onSubmit }) => {
     const [selectedPackage, setSelectedPackage] = useState('')
     const [packages, setPackages] = useState([]);
 
-    useEffect(() => {
-        axios.get('/api/packages', {
-            params: {
-                role: user.role,
-                plan: plan
-            }
-        }).then(res => setPackages(res.data)).catch(console.error)
-    }, [])
+  useEffect(() => {
+    axios.get('/api/packages', {
+      params: {
+        role: user.role,
+        plan: plan
+      }
+    }).then(res => setPackages(res.data)).catch(console.error);
+  }, [plan, user.role]);
 
-    return (
-        <div>
-            <div className="w-full p-4">
-                <div className={`mb-6 ${styles.pricePlanContainer}`}>
-                    {packages.map((_package, index) => (
-                        <div
-                            key={index}
-                            className={`relative p-4 mb-8 flex items-center gap-12 cursor-pointer border primary-border rounded-lg ${selectedPackage === _package._id ? "hover-shadow-dark" : "hover-shadow-light"
-                                }`}
-                            onClick={() => setSelectedPackage(_package._id)}
-                        >
-                            {selectedPackage === _package._id && (
-                                <img
-                                    src={'/assets/checkmark.png'}
-                                    alt="Selected"
-                                    className="absolute -left-6 w-16 h-16"
-                                />
-                            )}
-                            <div className={`blueBackground py-10 px-12 ${styles.pricePlan}`}>
-                                <h3 className="text-white text-2xl mt-2">{_package.name}</h3>
-                                <p className="text-primary text-3xl font-bold">
-                                    ${_package.amount / 100}
-                                </p>
-                            </div>
-                            <div>
-                                <ul className="text-zinc-400">
-                                    <li className="mb-4 text-white">
-                                        <span className="text-primary">1. Performance Tracking:</span> Monitor player statistics and
-                                        trends to identify strengths, weaknesses, and areas for
-                                        improvement.
-                                    </li>
-                                    <li className="mb-4 text-white">
-                                        <span className="text-primary">2. Tactical Insights:</span> Analyze gameplay patterns to
-                                        optimize strategies and adapt game plans for competitive
-                                        advantage.
-                                    </li>
-                                    <li className="mb-4 text-white">
-                                        <span className="text-primary">3. Injury Prevention:</span> Utilize data to manage player
-                                        workload, track fatigue levels, and reduce the risk of
-                                        injuries through informed decision-making.
-                                    </li>
-                                    <li className="text-white">
-                                        <span className="text-primary">4. Talent Development:</span> Identify promising players, track
-                                        their progress, and tailor training programs to maximize
-                                        their potential.
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-                <div className="flex space-x-4 justify-end mt-4">
-                    {onBack &&
-                        <button
-                            className="bg-white text-black font-bold px-4 py-1 rounded"
-                            onClick={onBack}
-                        >
-                            BACK
-                        </button>
-                    }
-                    <button
-                        className={`px-4 py-1 rounded font-bold bg-primary dark-blue-color hover-button-shadow`}
-                        onClick={() => onSubmit(packages.find(p => p._id === selectedPackage))}
-                        disabled={!selectedPackage}
-                    >
-                        CONTINUE
-                    </button>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="w-4/5	mx-auto p-4">
+        <div className="text-center mb-8">
+          <h2 className="text-white text-3xl mb-2">Pick your Membership</h2>
+          <p className="text-primary">1 Credit = 1 Throw</p>
         </div>
-    );
+        <div className={`grid grid-cols-1 md:grid-cols-2 gap-8 ${styles.pricePlanContainer}`}>
+          {packages.map((_package, index) => (
+            <div
+              key={index}
+              className={`relative p-4 flex flex-col items-center text-center cursor-pointer blueBackground rounded-lg ${selectedPackage === _package._id ? "hover-shadow-dark" : "hover-shadow-light"
+                }`}
+              onClick={() => setSelectedPackage(_package._id)}
+            >
+              {selectedPackage === _package._id && (
+                <img
+                  src={'/assets/checkmark.png'}
+                  alt="Selected"
+                  className="absolute top-4 right-4 w-8 h-8"
+                />
+              )}
+              <div className={`py-8`}>
+                <h3 className="text-white text-3xl mb-2">{_package.name}</h3>
+                <p className="text-primary text-4xl font-bold">
+                  ${_package.amount / 100}
+                </p>
+                <p className="text-white mt-2">
+                    {_package.description}
+                </p>
+                 <p className="text-white mt-2">
+                  {_package.throwsPerMonth 
+                    ? `${_package.throwsPerMonth} throws per month & additional credits available at` 
+                    : "Credits are available at"}
+                    <span className="text-primary"> ${_package.amountPerCredit / 100}/credit</span>
+                </p>
+                
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="flex space-x-4 justify-end mt-8">
+          {onBack && (
+            <button
+              className="bg-white text-black font-bold px-4 py-2 rounded"
+              onClick={onBack}
+            >
+              BACK
+            </button>
+          )}
+          <button
+            className={`px-4 py-2 rounded font-bold bg-primary dark-blue-color hover-button-shadow`}
+            onClick={() => onSubmit(packages.find(p => p._id === selectedPackage))}
+            disabled={!selectedPackage}
+          >
+            CONTINUE
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PickYourMembership;
